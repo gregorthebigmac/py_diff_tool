1c0d9c0212978dc56c0053bae3d407d4e876bb99
gregorthebigmac <gregorthebigmac@gmail.com>
2018-11-28 07:39:08 -0600

    getting there!

--- diff_tool.py
+++ diff_tool.py
@@ -1,12 +1,12 @@
 import os
 
-root_dir = raw_input("What dir?")
-dir_path, dirs, filenames = os.walk(root_dir)
-print(dir_path)
-print("contents:")
-print("dirs:")
-print(dirs)
-print("files:")
-print(filenames)
+def get_dir(m_root_dir, m_sub_dirs, m_filenames):
+	for m_root_dir, m_sub_dirs, m_filenames in os.walk(m_root_dir):
+		m_sub_dirs.sort()
+		for m_sub_sub_dirs in m_sub_dirs:
+			m_sub_sub_dirs.sort()
+			m_filenames.sort()
+	return m_sub_dirs, m_filenames
 
-
+root_dir = raw_input("what path?")
+root_dir, sub_dirs, filenames = get_dir(root_dir, sub_dirs, filenames)
--- diff_tool_old.py
+++ diff_tool_old.py
@@ -4,10 +4,14 @@ for dirname, dirnames, filenames in os.walk('.'):
 	# print path to all subdirectories first
 	for subdirname in dirnames:
 		print(os.path.join(dirname, subdirname))
+		print('subdirname = ', type(subdirname))
+		print('dirname = ', type(dirname))
 
 	# print path to all filenames
 	for filename in filenames:
 		print(os.path.join(dirname, filename))
+		print('filename = ', type(filename))
+		print('filenames = ', type(filenames))
 
 	# advanced usage:
 	# editing the 'dirnames' list will stop os.walk() from recursing into there
